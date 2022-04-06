b8d34d0825b9d81c9a58ec22e84c437514682663
Ion Griu <ion.griu@codefactorygroup.com>
2022-04-06 18:45:47 +0300

    Added a test method

--- src/App.java
+++ src/App.java
@@ -1,6 +1,10 @@
 public class App {
 
 
+    public static void testMethod(){
+        System.out.println("test !");
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello from git!");
     }
